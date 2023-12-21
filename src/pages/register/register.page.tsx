6776ee4a428b247402c8debdfaa39700809e4212
Guille <g.mirandagaleote@gmail.com>
2023-12-21 04:39:03 +0100

    New register

--- src/pages/register/register.page.tsx
+++ src/pages/register/register.page.tsx
@@ -1,10 +1,9 @@
-import { HeaderSession } from "../../features/users/components/header.session/header.session";
+
 import { Register } from "../../features/users/components/register/register";
 
 export default function RegisterPage() {
   return (
     <>
-      <HeaderSession></HeaderSession>
       <Register></Register>;
     </>
   );
